34945827fdc55c1a76bda438e738e5599823858c
luzinaldocruz <luzinaldocruz@gmail.com>
2024-09-15 21:49:35 -0300

    add deploy to heroku

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
-const express = require('express');
-const path = require('path');
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'dist')));
 
@@ -8,7 +14,6 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
-const port = process.env.PORT || 8080;
 app.listen(port, () => {
-  console.log(`App running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
